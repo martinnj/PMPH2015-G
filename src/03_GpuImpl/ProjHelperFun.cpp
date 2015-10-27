decf1c3eb30b60629f38633b6a7a10257a5718e1
Henrik <henrik.bendt@gmail.com>
2015-10-27 15:06:32 +0100

    reversed to working state, globs not flat.

--- src/03_GpuImpl/ProjHelperFun.cpp
+++ src/03_GpuImpl/ProjHelperFun.cpp
@@ -50,52 +50,70 @@ void initGrid(  const REAL s0, const REAL alpha, const REAL nu,const REAL t,
 void initOperator(  const vector<REAL>& x,
                     vector<vector<REAL> >& Dxx
 ) {
-	const unsigned n = x.size();
+    const unsigned n = x.size();
 
-	REAL dxl, dxu;
+    REAL dxl, dxu;
 
-	//	lower boundary
-	dxl		 =  0.0;
-	dxu		 =  x[1] - x[0];
+    //  lower boundary
+    dxl      =  0.0;
+    dxu      =  x[1] - x[0];
 
-	Dxx[0][0] =  0.0;
-	Dxx[0][1] =  0.0;
-	Dxx[0][2] =  0.0;
+    Dxx[0][0] =  0.0;
+    Dxx[0][1] =  0.0;
+    Dxx[0][2] =  0.0;
     Dxx[0][3] =  0.0;
 
-	//	standard case
+    //  standard case
     // Can be parallelized directly as each iteration writes to independent
     // Dxx indices. x is only read, so each iteration is independent.
     // x could be put in shared memory.
-	for(unsigned i=1;i<n-1;i++) // par
-	{
-		dxl      = x[i]   - x[i-1];
-		dxu      = x[i+1] - x[i];
+    for(unsigned i=1;i<n-1;i++) // par
+    {
+        dxl      = x[i]   - x[i-1];
+        dxu      = x[i+1] - x[i];
 
-		Dxx[i][0] =  2.0/dxl/(dxl+dxu);
-		Dxx[i][1] = -2.0*(1.0/dxl + 1.0/dxu)/(dxl+dxu);
-		Dxx[i][2] =  2.0/dxu/(dxl+dxu);
+        Dxx[i][0] =  2.0/dxl/(dxl+dxu);
+        Dxx[i][1] = -2.0*(1.0/dxl + 1.0/dxu)/(dxl+dxu);
+        Dxx[i][2] =  2.0/dxu/(dxl+dxu);
         Dxx[i][3] =  0.0;
-	}
+    }
 
-	//	upper boundary
-	dxl		   =  x[n-1] - x[n-2];
-	dxu		   =  0.0;
+    //  upper boundary
+    dxl        =  x[n-1] - x[n-2];
+    dxu        =  0.0;
 
-	Dxx[n-1][0] = 0.0;
-	Dxx[n-1][1] = 0.0;
-	Dxx[n-1][2] = 0.0;
+    Dxx[n-1][0] = 0.0;
+    Dxx[n-1][1] = 0.0;
+    Dxx[n-1][2] = 0.0;
     Dxx[n-1][3] = 0.0;
 }
 
 
-void transpose(vector<vector<REAL> > MIn,
+void transposeVect(vector<vector<REAL> > MIn,
                vector<vector<REAL> >* MOut,
                unsigned int M,
-               unsigned int N) {
+               unsigned int N) 
+{
     for (int i = 0 ; i < M ; i++) {
         for (int j = 0 ; j < N ; j++) {
             (*MOut)[i][j] = MIn[j][i];
        }
    }
+}
+
+void transpose(REAL* A, REAL** B, int M, int N) {
+    for(int i = 0 ; i < M ; i++) {
+        for(int j = 0 ; j < N ; j++) {
+            //(*B)[j*M+i] = A[i*N+j];
+            (*B)[i*N+j] = A[j*M+i];
+        }
+    }
+}
+
+// row = row idx
+// col = col idx
+// width = number of columns in the matrix
+// ex: A[row,col] = A[idx2d(row, col, a.cols)]
+unsigned int idx2d(int row, int col, int width) {
+    return row * width + col;
 }
--- src/03_GpuImpl/ProjHelperFun.h
+++ src/03_GpuImpl/ProjHelperFun.h
@@ -9,24 +9,23 @@
 
 using namespace std;
 
-
 struct PrivGlobs {
 
-    //	grid
+    //  grid
     vector<REAL>        myX;        // [numX]
     vector<REAL>        myY;        // [numY]
     vector<REAL>        myTimeline; // [numT]
     unsigned            myXindex;
     unsigned            myYindex;
 
-    //	variable
+    //  variable
     vector<vector<REAL> > myResult; // [numX][numY]
 
-    //	coeffs
+    //  coeffs
     vector<vector<REAL> >   myVarX; // [numX][numY]
     vector<vector<REAL> >   myVarY; // [numX][numY]
 
-    //	operators
+    //  operators
     vector<vector<REAL> >   myDxx;  // [numX][4]
     vector<vector<REAL> >   myDyy;  // [numY][4]
 
@@ -65,6 +64,94 @@ struct PrivGlobs {
 } __attribute__ ((aligned (128)));
 
 
+/*
+struct PrivGlobs {
+
+    //  grid
+    REAL*    myX;        // [numX]
+    unsigned myXsize;
+
+    REAL*    myY;        // [numY]
+    unsigned myYsize;
+
+    REAL*    myTimeline; // [numT]
+    unsigned myTimelineSize;
+
+    unsigned myXindex;
+    unsigned myYindex;
+
+    //  variable
+    //vector<vector<REAL> > myResult; // [numX][numY]
+    REAL* myResult;
+    unsigned myResultRows;
+    unsigned myResultCols;
+
+    //  coeffs
+    //vector<vector<REAL> >   myVarX; // [numX][numY]
+    REAL* myVarX;
+    unsigned myVarXRows;
+    unsigned myVarXCols;
+
+    //vector<vector<REAL> >   myVarY; // [numX][numY]
+    REAL* myVarY;
+    unsigned myVarYRows;
+    unsigned myVarYCols;
+
+    //  operators
+    //vector<vector<REAL> >   myDxx;  // [numX][4]
+    REAL* myDxx;
+    unsigned myDxxRows;
+    unsigned myDxxCols;
+
+    //vector<vector<REAL> >   myDyy;  // [numY][4]
+    REAL* myDyy;
+    unsigned myDyyRows;
+    unsigned myDyyCols;
+
+    PrivGlobs( ) {
+        printf("Invalid Contructor: need to provide the array sizes! EXITING...!\n");
+        exit(0);
+    }
+
+    PrivGlobs(  const unsigned int& numX,
+                const unsigned int& numY,
+                const unsigned int& numT ) {
+
+        this->myX = (REAL*) malloc(sizeof(REAL)*numX);
+        this->myXsize = numX;
+
+        //this->myDxx.resize(numX);
+        this->myDxx = (REAL*) malloc(sizeof(REAL)*(numX*4));
+        this->myDxxRows = numX;
+        this->myDxxCols = 4;
+
+        this->myY = (REAL*) malloc(sizeof(REAL)*numY);
+        this->myYsize = numY;
+
+        //this->myDyy.resize(numY);
+        this->myDyy = (REAL*) malloc(sizeof(REAL)*(numY*4));
+        this->myDyyRows = numY;
+        this->myDyyCols = 4;
+
+        this->myTimeline = (REAL*) malloc(sizeof(REAL)*numT);
+        this->myTimelineSize = numT;
+
+        this->myResult = (REAL*) malloc(sizeof(REAL)*(numX*numY));
+        this->myResultRows = numX;
+        this->myResultCols = numY;
+
+        this->myVarX = (REAL*) malloc(sizeof(REAL)*(numX*numY));
+        this->myVarXRows = numX;
+        this->myVarXCols = numY;
+
+        this->myVarY = (REAL*) malloc(sizeof(REAL)*(numX*numY));
+        this->myVarYRows = numX;
+        this->myVarYCols = numY;
+
+    }
+} __attribute__ ((aligned (128)));
+*/
+
 void initGrid(  const REAL s0, const REAL alpha, const REAL nu,const REAL t,
                 const unsigned numX, const unsigned numY, const unsigned numT, PrivGlobs& globs
             );
@@ -77,29 +164,7 @@ void updateParams(const unsigned g, const REAL alpha, const REAL beta, const REA
 
 void setPayoff(const REAL strike, PrivGlobs& globs );
 
-void tridag(
-    const vector<REAL>&   a,   // size [n]
-    const vector<REAL>&   b,   // size [n]
-    const vector<REAL>&   c,   // size [n]
-    const vector<REAL>&   r,   // size [n]
-    const int             n,
-          vector<REAL>&   u,   // size [n]
-          vector<REAL>&   uu   // size [n] temporary
-);
-
 void rollback( const unsigned g, PrivGlobs& globs );
-
-REAL   value(   PrivGlobs    globs,
-                const REAL s0,
-                const REAL strike,
-                const REAL t,
-                const REAL alpha,
-                const REAL nu,
-                const REAL beta,
-                const unsigned int numX,
-                const unsigned int numY,
-                const unsigned int numT
-            );
 
 void run_GPU(
                 const unsigned int&   outer,
@@ -116,9 +181,16 @@ void run_GPU(
 
 
 //void transpose(REAL** MIn, REAL** MOut, int M, int N);
-void transpose(vector<vector<REAL> > MIn,
+void transposeVect(vector<vector<REAL> > MIn,
                vector<vector<REAL> >* MOut,
                unsigned int M,
                unsigned int N);
+
+void transpose(REAL* A, REAL** B, int M, int N);
+
+
+unsigned int idx2d(int row, int col, int width);
+
+
 
 #endif // PROJ_HELPER_FUNS
